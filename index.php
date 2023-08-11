e146ce42d92d73755c1ac76f73e485a5288e04da
AnahiMamanicls <narielanahi@gmail.com>
2023-08-11 16:07:12 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,, viewport-fit=cover">
+    <meta name="color-scheme" content="light dark">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <SCRipt scr="js/jquery.js"></SCRipt>
+    <script scr="bootstrap/js/bootstrap.js"></script>
+    <script scr="js/funcoes.js"></script>
+    <title>SISTEMMA DE AGENDAMENTO - CLIENTE</title>
+</head>
+<body>
+    
+</body>
+</html>
